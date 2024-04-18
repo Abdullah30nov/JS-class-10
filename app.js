91c592ac65f4dcff2c4517f61383bcf647dfdf20
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-04-18 21:21:24 +0500

    Calcultor & DOM start

---
+++ app.js
@@ -0,0 +1,35 @@
+// console.log(document)
+
+// function over(){
+//     var img = document.getElementById('over');
+// }
+
+// function submit(){
+// var username = document.getElementById('usernanme').value;
+// var password = document.getElementById('password').value;
+
+// console.log(username.value)
+// console.log(password.value)
+// }
+// submit()
+
+                                 //Read more & R ead less
+// function expandpara(){
+//     var short = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, doloribus!";
+//     var complete = " Loremipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus quidem ea magnam dolore illum expedita quam, et quibusdam est quasi enim voluptates unde iusto amet pariatur praesentium ex minus recusandae aut. Obcaecati totam aut rem culpa, architecto fugit ullam!"
+//     var para = document.getElementById('para');
+//     var link = document.getElementById('link');
+    
+//     if(link.innerHTML === "Read more"){
+//         para.innerHTML = complete
+//         link.innerHTML = "Read Less"
+//     }
+//     else{
+//         para.innerHTML = short;
+//         link.innerHTML = "Read more"
+//     }
+// }
+
+
+
+
